679c8ce37abeb6ff968562ff3aa2d374108edf77
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-08-08 12:49:47 -0300

    Configuração do  cors

--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,7 @@ const connectionsController = new ConnectionsController();
 routes.get('/classes', classesControlers.index)
 routes.post('/classes', classesControlers.create)
 
-// routes.get('/connections', connectionsController.index)
+routes.get('/connections', connectionsController.index)
 routes.post('/connections', connectionsController.create)
 
 export default routes;
